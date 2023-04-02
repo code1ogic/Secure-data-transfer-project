2cf7214e746c47d6b88c8ac9ddc58c4c6b613a96
code1ogic <96641204+code1ogic@users.noreply.github.com>
2023-04-02 08:27:07 +0530

    Update login.component.ts

--- frontend/src/app/component/auth/login/login.component.ts
+++ frontend/src/app/component/auth/login/login.component.ts
@@ -15,17 +15,23 @@ export class LoginComponent implements OnInit {
 
   @ViewChild('modal')
   modal: ElementRef | undefined;
-
+  isLogingSuccess : boolean = false;
   userObject : User = {
     _id: '',
     email: '',
     name: '',
     password: ''
   };
+  count : number = 3;
   response : String ='';
   userLoginData = new UntypedFormGroup({
     email : new UntypedFormControl(),
     password : new UntypedFormControl()
+  })
+  
+  userOTP = new UntypedFormGroup({
+    otp : new UntypedFormControl(),
+    userId : new UntypedFormControl()
   })
   showPassword : boolean = false;
 
@@ -35,6 +41,9 @@ export class LoginComponent implements OnInit {
     this.userLoginData = this.fb.group({
       email : ['',[Validators.required, Validators.email]],
       password : ['',[Validators.required]]
+    });
+    this.userOTP = this.fb.group({
+      otp : ['',[Validators.required]]
     });
     this.response = "";
   }
@@ -46,13 +55,39 @@ export class LoginComponent implements OnInit {
     this.auth.login(this.userObject).subscribe(res => {
       console.log(res);
       localStorage.setItem("user_id", res._id);
+      this.isLogingSuccess = true;
+      //this.route.navigate(['/dashboard']);
+    }, err => {
+      this.response = err.error.msg;
+      this.modal?.nativeElement.click();
+    })
+  }
+  
+  verifyOtp() {
+    if(this.count < 1) {
+      this.response = "You have reached maximum attempts for verifying OTP";
+      this.modal?.nativeElement.click();
+      return;
+    }
+    
+    this.userObject.otp = this.userOTP.value.otp;
+    this.userObject.userId = localStorage.getItem("user_id");
+    this.count = this.count - 1;
+    this.auth.verifyOTP(this.userObject).subscribe(res => {
       this.route.navigate(['/dashboard']);
     }, err => {
       this.response = err.error.msg;
       this.modal?.nativeElement.click();
     })
   }
-
+  
+  closeModel() {
+    if(this.count < 1) {
+      this.route.navigate(['/']);
+      return;
+    }
+  }
+  
   changePasswordProperty() {
     this.showPassword = !this.showPassword;
   }
